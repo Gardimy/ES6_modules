d640fa18bca73aaff5a52383a01187ea76bf6ef0
Gardimy Charles <gardimycharles889@gmail.com>
2023-05-08 17:58:20 -0400

    Add navigaton.js file

---
+++ modules/navigaton.js
@@ -0,0 +1,40 @@
+export const addBookForm = document.getElementById('add-book-form');
+
+export const bookList = document.getElementById('book-list');
+
+export const listButton = document.querySelector('#list');
+export const addBookButton = document.querySelector('#addBook');
+export const contactButton = document.querySelector('#contact');
+
+export const listContainer = document.querySelector('.list_container');
+export const formSection = document.querySelector('.form_section');
+export const contactSection = document.querySelector('.contact_section');
+
+// const showList = () => {
+//   document.querySelector('.list_container').classList.add('dBlock');
+//   document.querySelector('.list_container').classList.remove('dNone');
+//   document.querySelector('.contact_section').classList.add('dNone');
+//   document.querySelector('.contact_section').classList.remove('dBlock');
+//   document.querySelector('.form_section').classList.add('dNone');
+//   document.querySelector('.form_section').classList.remove('dBlock');
+// };
+
+// const showAddBookForm = () => {
+//   document.querySelector('.list_container').classList.add('dNone');
+//   document.querySelector('.list_container').classList.remove('dBlock');
+//   document.querySelector('.form_section').classList.add('dBlock');
+//   document.querySelector('.form_section').classList.remove('dNone');
+//   document.querySelector('.contact_section').classList.add('dNone');
+//   document.querySelector('.contact_section').classList.remove('dBlock');
+// };
+
+// const showContactSection = () => {
+//   document.querySelector('.contact_section').classList.add('dBlock');
+//   document.querySelector('.contact_section').classList.remove('dNone');
+//   document.querySelector('.form_section').classList.add('dNone');
+//   document.querySelector('.form_section').classList.remove('dBlock');
+//   document.querySelector('.list_container').classList.add('dNone');
+//   document.querySelector('.list_container').classList.remove('dBlock');
+// };
+
+// export { showList, showAddBookForm, showContactSection };

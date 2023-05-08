1f126c7c394706d537dcedb4539c214dec795bea
Gardimy Charles <gardimycharles889@gmail.com>
2023-05-08 18:10:04 -0400

    Update navigaton.js file

--- modules/navigaton.js
+++ modules/navigaton.js
@@ -9,32 +9,3 @@ export const contactButton = document.querySelector('#contact');
 export const listContainer = document.querySelector('.list_container');
 export const formSection = document.querySelector('.form_section');
 export const contactSection = document.querySelector('.contact_section');
-
-// const showList = () => {
-//   document.querySelector('.list_container').classList.add('dBlock');
-//   document.querySelector('.list_container').classList.remove('dNone');
-//   document.querySelector('.contact_section').classList.add('dNone');
-//   document.querySelector('.contact_section').classList.remove('dBlock');
-//   document.querySelector('.form_section').classList.add('dNone');
-//   document.querySelector('.form_section').classList.remove('dBlock');
-// };
-
-// const showAddBookForm = () => {
-//   document.querySelector('.list_container').classList.add('dNone');
-//   document.querySelector('.list_container').classList.remove('dBlock');
-//   document.querySelector('.form_section').classList.add('dBlock');
-//   document.querySelector('.form_section').classList.remove('dNone');
-//   document.querySelector('.contact_section').classList.add('dNone');
-//   document.querySelector('.contact_section').classList.remove('dBlock');
-// };
-
-// const showContactSection = () => {
-//   document.querySelector('.contact_section').classList.add('dBlock');
-//   document.querySelector('.contact_section').classList.remove('dNone');
-//   document.querySelector('.form_section').classList.add('dNone');
-//   document.querySelector('.form_section').classList.remove('dBlock');
-//   document.querySelector('.list_container').classList.add('dNone');
-//   document.querySelector('.list_container').classList.remove('dBlock');
-// };
-
-// export { showList, showAddBookForm, showContactSection };
